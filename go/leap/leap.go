bf89d0b08793b94135616414dd9816c654f4d7f5
erick patrick <github@erickpatrick.net>
2026-07-06 22:02:25 +0200

    solves go/leap, first try

---
+++ go/leap/leap.go
@@ -0,0 +1,20 @@
+// This is a "stub" file.  It's a little start on your solution.
+// It's not a complete solution though; you have to write some code.
+
+// Package leap should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package leap
+
+// IsLeapYear should have a comment documenting it.
+func IsLeapYear(year int) bool {
+	switch {
+	case year%400 == 0:
+		return true
+	case year%100 == 0:
+		return false
+	case year%4 == 0:
+		return true
+	default:
+		return false
+	}
+}
