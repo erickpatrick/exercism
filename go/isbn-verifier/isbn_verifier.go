ec56b495425780c8ef0f7d4c73640d739c183515
erick patrick <github@erickpatrick.net>
2026-07-11 23:56:13 +0200

    unsolved go/isbn_Verifier, needs more tries

--- go/isbn-verifier/isbn_verifier.go
+++ go/isbn-verifier/isbn_verifier.go
@@ -1,5 +1,30 @@
 package isbnverifier
 
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
 func IsValidISBN(isbn string) bool {
-	panic("Please implement the IsValidISBN function")
+	cleanISBN := strings.ReplaceAll(isbn, "-", "")
+	if len(cleanISBN) == 0 {
+		return false
+	}
+
+	checker := regexp.MustCompile(`^\d{9}[\dX]{1}$`)
+	if doesISBNContainValidChars := checker.MatchString(cleanISBN); !doesISBNContainValidChars {
+		return false
+	}
+
+	sum := 0
+	multiplier := 10
+	i := 0
+
+	for {
+	}
+
+	fmt.Println(cleanISBN)
+
+	return sum%11 == 0
 }
