d8ddc929eb9c3ddb19a2125562bb2346b77c6fd1
erick patrick <github@erickpatrick.net>
2026-07-29 20:16:28 +0200

    unsolved go/pov - passes "Results in the same tree if the input tree is a singleton"

--- go/pov/pov.go
+++ go/pov/pov.go
@@ -43,7 +43,11 @@ func (tr *Tree) String() string {
 
 // FromPov returns the pov from the node specified in the argument.
 func (tr *Tree) FromPov(from string) *Tree {
-	panic("Please implement this function")
+	if tr.root == from {
+		return tr
+	}
+
+	return tr
 }
 
 // PathTo returns the shortest path between two nodes in the tree.
