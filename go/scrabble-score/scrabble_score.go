c78dd2f741eebbaffe92c1f95f69664f8a6af40a
erick patrick <github@erickpatrick.net>
2026-08-10 09:50:21 +0200

    solved go/scrabble-score

---
+++ go/scrabble-score/scrabble_score.go
@@ -0,0 +1,29 @@
+package scrabblescore
+
+import "unicode"
+
+func Score(word string) int {
+	total := 0
+	for _, letter := range word {
+		switch unicode.ToUpper(letter) {
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+			total += 1
+		case 'D', 'G':
+			total += 2
+		case 'B', 'C', 'M', 'P':
+			total += 3
+		case 'F', 'H', 'V', 'W', 'Y':
+			total += 4
+		case 'K':
+			total += 5
+		case 'J', 'X':
+			total += 8
+		case 'Q', 'Z':
+			total += 10
+		default:
+			total += 0
+		}
+	}
+
+	return total
+}
