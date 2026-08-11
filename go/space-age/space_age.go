0b8a3d6124af809c31b4a9486b38727f36a4dfd2
erick patrick <github@erickpatrick.net>
2026-08-11 12:12:09 +0200

    solved go/space-age - refactors to simplify code

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -1,26 +1,24 @@
 package spaceage
 
-import "strings"
-
 type Planet string
 
 func Age(seconds float64, planet Planet) float64 {
 	const EarthDays = 365.25
 	const SecondsInADay = 60.0 * 60.0 * 24.0
-	rate := map[string]float64{
-		"mercury": 0.2408467,
-		"venus":   0.61519726,
-		"earth":   1.0,
-		"mars":    1.8808158,
-		"jupiter": 11.862615,
-		"saturn":  29.447498,
-		"uranus":  84.016846,
-		"neptune": 164.79132,
+	rate := map[Planet]float64{
+		"Mercury": 0.2408467,
+		"Venus":   0.61519726,
+		"Earth":   1.0,
+		"Mars":    1.8808158,
+		"Jupiter": 11.862615,
+		"Saturn":  29.447498,
+		"Uranus":  84.016846,
+		"Neptune": 164.79132,
 	}
 
-	if rate[strings.ToLower(string(planet))] == 0.0 {
+	if rate[planet] == 0.0 {
 		return -1.0
 	}
 
-	return seconds / SecondsInADay / (rate[strings.ToLower(string(planet))] * EarthDays)
+	return seconds / SecondsInADay / (rate[planet] * EarthDays)
 }
