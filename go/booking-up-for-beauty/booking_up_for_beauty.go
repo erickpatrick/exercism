013637b0e23d27553518948478011923b9f3cebf
erick patrick <github@erickpatrick.net>
2026-08-13 22:33:21 +0200

    unsolved go/booking-up-for-beauty - solves IsAfternoonAppointment

--- go/booking-up-for-beauty/booking_up_for_beauty.go
+++ go/booking-up-for-beauty/booking_up_for_beauty.go
@@ -22,7 +22,9 @@ func HasPassed(date string) bool {
 
 // IsAfternoonAppointment returns whether a time is in the afternoon.
 func IsAfternoonAppointment(date string) bool {
-	panic("Please implement the IsAfternoonAppointment function")
+	t, _ := time.Parse("Monday, January 2, 2006 15:04:05", date)
+
+	return t.Hour() >= 12 && t.Hour() < 18
 }
 
 // Description returns a formatted string of the appointment time.
