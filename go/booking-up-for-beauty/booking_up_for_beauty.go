2d9cc32bbd1f57fc3f0b1193a01992c69dd17fb3
erick patrick <github@erickpatrick.net>
2026-08-13 22:29:11 +0200

    unsolved go/booking-up-for-beauty - solves HasPassed

--- go/booking-up-for-beauty/booking_up_for_beauty.go
+++ go/booking-up-for-beauty/booking_up_for_beauty.go
@@ -15,7 +15,9 @@ func Schedule(date string) time.Time {
 
 // HasPassed returns whether a date has passed.
 func HasPassed(date string) bool {
-	panic("Please implement the HasPassed function")
+	t, _ := time.Parse("January 2, 2006 15:04:05", date)
+
+	return time.Now().After(t)
 }
 
 // IsAfternoonAppointment returns whether a time is in the afternoon.
