ab60eb0942781c7d2fd6e16941c0c64dfa56e26a
erick patrick <github@erickpatrick.net>
2026-08-16 20:10:45 +0200

    unsolved go/chessboard - solve CountInFile

---
+++ go/chessboard/chessboard.go
@@ -0,0 +1,43 @@
+package chessboard
+
+type File []bool
+
+type Chessboard map[string]File
+
+// CountInFile returns how many squares are occupied in the chessboard,
+// within the given file.
+func CountInFile(cb Chessboard, file string) int {
+	occupied := 0
+
+	if len(cb) == 0 {
+		return 0
+	}
+
+	if file < "A" && file > "H" {
+		return 0
+	}
+
+	for _, value := range cb[file] {
+		if value {
+			occupied++
+		}
+	}
+
+	return occupied
+}
+
+// CountInRank returns how many squares are occupied in the chessboard,
+// within the given rank.
+func CountInRank(cb Chessboard, rank int) int {
+	panic("Please implement CountInRank()")
+}
+
+// CountAll should count how many squares are present in the chessboard.
+func CountAll(cb Chessboard) int {
+	panic("Please implement CountAll()")
+}
+
+// CountOccupied returns how many squares are occupied in the chessboard.
+func CountOccupied(cb Chessboard) int {
+	panic("Please implement CountOccupied()")
+}
