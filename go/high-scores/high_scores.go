a6cd93d7fa04191335df21ea7996c96f759e00bb
erick patrick <github@erickpatrick.net>
2026-08-19 22:46:02 +0200

    unsolved go/high-scores - solves HighScores

---
+++ go/high-scores/high_scores.go
@@ -0,0 +1,30 @@
+package highscores
+
+type HighScores struct {
+	scores []int
+}
+
+// NewHighScores returns a new HighScores object.
+func NewHighScores(scores []int) *HighScores {
+	return &HighScores{scores: scores}
+}
+
+// Scores returns all the scores.
+func (s *HighScores) Scores() []int {
+	panic("Please implement the Scores function")
+}
+
+// Latest returns the latest (last) score.
+func (s *HighScores) Latest() int {
+	panic("Please implement the Latest function")
+}
+
+// PersonalBest returns the best (highest) score.
+func (s *HighScores) PersonalBest() int {
+	panic("Please implement the PersonalBest function")
+}
+
+// TopThree returns the top three scores.
+func (s *HighScores) TopThree() []int {
+	panic("Please implement the TopThree function")
+}
