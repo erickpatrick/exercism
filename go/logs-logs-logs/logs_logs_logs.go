b6bb87e12fdcd43755ba66e6e01ee2f8882ee480
erick patrick <github@erickpatrick.net>
2026-08-20 22:39:35 +0200

    unsolved go/logs-logs-logs - solves Replace

--- go/logs-logs-logs/logs_logs_logs.go
+++ go/logs-logs-logs/logs_logs_logs.go
@@ -1,5 +1,7 @@
 package logs
 
+import "strings"
+
 // Application identifies the application emitting the given log.
 func Application(log string) string {
 	for _, r := range log {
@@ -19,7 +21,7 @@ func Application(log string) string {
 // Replace replaces all occurrences of old with new, returning the modified log
 // to the caller.
 func Replace(log string, oldRune, newRune rune) string {
-	panic("Please implement the Replace() function")
+	return strings.ReplaceAll(log, string(oldRune), string(newRune))
 }
 
 // WithinLimit determines whether or not the number of characters in log is
