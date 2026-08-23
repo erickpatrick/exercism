e82a96eb27f57e81375719430329a395602a0745
erick patrick <github@erickpatrick.net>
2026-08-23 15:50:59 +0200

    unsolved go/census - solved HasRequiredInfo

--- go/census/census.go
+++ go/census/census.go
@@ -15,7 +15,7 @@ func NewResident(name string, age int, address map[string]string) *Resident {
 
 // HasRequiredInfo determines if a given resident has all of the required information.
 func (r *Resident) HasRequiredInfo() bool {
-	panic("Please implement HasRequiredInfo.")
+	return r.Name != "" && r.Age >= 0 && r.Address != nil && r.Address["street"] != ""
 }
 
 // Delete deletes a resident's information.
