eb8f2d06433aab110405147867b92b9caf7c0dd1
erick patrick <github@erickpatrick.net>
2026-08-23 16:13:25 +0200

    solves go/airport-robot

---
+++ go/airport-robot/airport_robot.go
@@ -0,0 +1,32 @@
+package airportrobot
+
+import "fmt"
+
+type Greeter interface {
+	LanguageName() string
+	Greet(name string) string
+}
+
+type Italian struct{}
+
+type Portuguese struct{}
+
+func SayHello(visitorName string, robotGreeter Greeter) string {
+	return fmt.Sprintf("I can speak %s: %s", robotGreeter.LanguageName(), robotGreeter.Greet(visitorName))
+}
+
+func (i Italian) Greet(name string) string {
+	return fmt.Sprintf("Ciao %s!", name)
+}
+
+func (i Italian) LanguageName() string {
+	return "Italian"
+}
+
+func (p Portuguese) Greet(name string) string {
+	return fmt.Sprintf("Olá %s!", name)
+}
+
+func (p Portuguese) LanguageName() string {
+	return "Portuguese"
+}
