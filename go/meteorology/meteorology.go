eca4be0ef3f9c011f73b8a51711e8ea040db9f53
erick patrick <github@erickpatrick.net>
2026-08-29 19:03:56 +0200

    unsolved go/meteorology - solved TemperatureUnit

---
+++ go/meteorology/meteorology.go
@@ -0,0 +1,59 @@
+package meteorology
+
+type TemperatureUnit int
+
+const (
+	Celsius    TemperatureUnit = 0
+	Fahrenheit TemperatureUnit = 1
+)
+
+// Add a String method to the TemperatureUnit type
+func (tu TemperatureUnit) String() string {
+	units := []string{"°C", "°F"}
+	return units[tu]
+}
+
+type Temperature struct {
+	degree int
+	unit   TemperatureUnit
+}
+
+// Add a String method to the Temperature type
+func (t Temperature) String() string {
+	return ""
+}
+
+type SpeedUnit int
+
+const (
+	KmPerHour    SpeedUnit = 0
+	MilesPerHour SpeedUnit = 1
+)
+
+// Add a String method to SpeedUnit
+func (su SpeedUnit) String() string {
+	return ""
+}
+
+type Speed struct {
+	magnitude int
+	unit      SpeedUnit
+}
+
+// Add a String method to Speed
+func (s Speed) String() string {
+	return ""
+}
+
+type MeteorologyData struct {
+	location      string
+	temperature   Temperature
+	windDirection string
+	windSpeed     Speed
+	humidity      int
+}
+
+// Add a String method to MeteorologyData
+func (md MeteorologyData) String() string {
+	return ""
+}
