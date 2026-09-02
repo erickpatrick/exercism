be3a7e831ca81621cf40d756104b0d157a438083
erick patrick <github@erickpatrick.net>
2026-09-02 21:01:05 +0200

    solved go/pangram - simplifies solution

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -9,16 +9,10 @@ func IsPangram(input string) bool {
 	alphabet := map[rune]bool{}
 	input = strings.ToLower(input)
 
-	if len(input) == 0 {
-		return false
-	}
-
 	for _, r := range input {
-		if !unicode.IsLetter(r) {
-			continue
+		if unicode.IsLetter(r) {
+			alphabet[r] = true
 		}
-
-		alphabet[r] = true
 	}
 
 	return len(alphabet) == 26
