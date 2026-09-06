90b277ed7827bac4d8e12ad91d76db2f419f9c09
erick patrick <github@erickpatrick.net>
2026-09-06 22:37:12 +0200

    unsolved go/grains - solved Square

---
+++ go/grains/grains.go
@@ -0,0 +1,17 @@
+package grains
+
+import (
+	"errors"
+	"math"
+)
+
+func Square(number int) (uint64, error) {
+	if number <= 0 || number > 64 {
+		return 0, errors.New("")
+	}
+	return uint64(math.Pow(float64(2), float64(number-1))), nil
+}
+
+func Total() uint64 {
+	panic("Please implement the Total function")
+}
