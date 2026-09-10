5e79b5fd0779b0608aff8272e5ae01f9cd8cb452
erick patrick <github@erickpatrick.net>
2026-09-10 16:58:32 +0200

    solved go/proverb - passes all tests

---
+++ go/proverb/proverb.go
@@ -0,0 +1,25 @@
+// This is a "stub" file.  It's a little start on your solution.
+// It's not a complete solution though; you have to write some code.
+
+// Package proverb should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package proverb
+
+import "fmt"
+
+// Proverb should have a comment documenting it.
+func Proverb(rhyme []string) []string {
+	length := len(rhyme)
+	result := []string{}
+
+	for i := range length {
+		if i+1 >= length {
+			result = append(result, fmt.Sprintf("And all for the want of a %s.", rhyme[0]))
+			break
+		}
+
+		result = append(result, fmt.Sprintf("For want of a %s the %s was lost.", rhyme[i], rhyme[i+1]))
+	}
+
+	return result
+}
---
+++ go/proverb/proverb_test.go
@@ -0,0 +1,25 @@
+package proverb
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestProverb(t *testing.T) {
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			actual := Proverb(tc.input)
+			if fmt.Sprintf("%q", actual) != fmt.Sprintf("%q", tc.expected) {
+				t.Fatalf("Proverb(%#v)\ngot: %#v\nwant: %#v", tc.input, actual, tc.expected)
+			}
+		})
+	}
+}
+
+func BenchmarkProverb(b *testing.B) {
+	for b.Loop() {
+		for _, test := range testCases {
+			Proverb(test.input)
+		}
+	}
+}
