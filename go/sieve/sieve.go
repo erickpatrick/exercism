7c316c0dbcaeab46d7b2a277d088e76ec30eb6b1
erick patrick <github@erickpatrick.net>
2026-09-16 23:12:42 +0200

    solved go/sieve - passes all tests

---
+++ go/sieve/sieve.go
@@ -0,0 +1,19 @@
+package sieve
+
+func Sieve(limit int) []int {
+	nums := []int{}
+	for i := 2; i <= limit; i++ {
+		add := true
+		for j := i - 1; j >= 2; j-- {
+			if i%j == 0 {
+				add = false
+			}
+		}
+
+		if add {
+			nums = append(nums, i)
+		}
+	}
+
+	return nums
+}
