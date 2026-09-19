44924f3eb8ae50b73d8a32f4df813f87359d5b57
erick patrick <github@erickpatrick.net>
2026-09-19 17:54:45 +0200

    solved go/eliuds-eggs - improves code

--- go/eliuds-eggs/eliuds_eggs.go
+++ go/eliuds-eggs/eliuds_eggs.go
@@ -3,14 +3,11 @@ package eliudseggs
 func EggCount(displayValue int) int {
 	result := 0
 
-	for {
+	for displayValue > 0 {
 		if displayValue%2 == 1 {
 			result += 1
 		}
 		displayValue /= 2
-		if displayValue == 0 {
-			break
-		}
 	}
 
 	return result
